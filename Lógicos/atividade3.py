1700c8d6d62507704f89244f4d98cd90f0ddb4da
Joyce Lima <joyce.slima@hotmail.com>
2024-06-15 14:46:36 -0300

    Atividade, semana 3

---
+++ Lógicos/atividade3.py
@@ -0,0 +1,7 @@
+nota = float(input('Digite a nota da aluna, de 0 a 10: '))
+frequencia = float(input('Digite a frequencia da aluna, de 0 a 100: '))
+
+if nota >=7 or frequencia >=75:
+    print('Aprovada!')
+else:
+    print('Reprovada!')    
